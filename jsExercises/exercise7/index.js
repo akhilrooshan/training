c01986a5c0539618bde34998a229703b4abbf9c6
Akhil roshan <akhil.a@pacewisdom.com>
2022-09-06 13:10:03 +0530

    e7

---
+++ jsExercises/exercise7/index.js
@@ -0,0 +1,15 @@
+function sepEach(arr)
+{
+    console.log("**********");
+
+    for (let i=0;i<arr.length;i++)
+    {
+        console.log("\n * "+arr[i]+" *" );
+    }
+    console.log("\n **********");
+}
+
+
+
+str=["ara","mds","ras"];
+sepEach(str);
