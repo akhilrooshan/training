b338ad6caee56bb1cd362d3877da837394192cb9
Akhil roshan <akhil.a@pacewisdom.com>
2022-09-08 10:39:47 +0530

    e8

--- jsExercises/exercise8/index.js
+++ jsExercises/exercise8/index.js
@@ -27,8 +27,31 @@ function englishToPIgLatin(str)
 
 function pigLatinToEnglish(str)
 {
+    str=str.toLowerCase();
+    const arr=str.split(" ");
+    let newArr=" ";
+    for (let i=0;i<arr.length;i++)
+    {
+        for(let j=0;j<arr[i].length;j++)
+        {
+            var o=arr[i].slice(0,-2);
+          
+            let m=o.slice(-1);
+         
+            let n=o.slice(0,-1)
+            var p=m+n;
+          
+             
+
+        }
+        newArr=newArr+" "+p;
+      
+    }
+    return newArr;
+
 }
 
 arrY="The quick brown fox";
-
+yArr="hetay uickqay rownbay oxfay";
 console.log(englishToPIgLatin(arrY));
+console.log(pigLatinToEnglish(yArr));
